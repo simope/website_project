3e7d4536545a54fd1222d1da6441bf1e4fba3f00
simope <simonepecora@live.it>
2024-08-09 15:14:01 +0100

    fix csrf problem between django and nginx

--- website/settings.py
+++ website/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-9$y3q=-*0ax))@f189*(j&mqm^qq0qt8t2@dkzq_gaf%pmyyds
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost']
+CSRF_TRUSTED_ORIGINS = ['https://localhost']
 
 # Application definition
 
