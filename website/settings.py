8f913ff92892be7f4773c902c124ddebee4f21f6
simope <simonepecora@live.it>
2024-08-13 10:33:53 +0100

    fix static files serving within docker images

--- website/settings.py
+++ website/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '../vol/static'
+STATIC_ROOT = '/vol/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'website/templates'),
     os.path.join(BASE_DIR, 'website/static'),
